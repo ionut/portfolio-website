63cdcc46335200bb9062e51455bcc4414691f0e2
Husoschi Ionut-Catalin <ionut.vi@supremedia.com>
2022-10-22 21:39:30 +0300

    about page done for mobile

--- js/script.js
+++ js/script.js
@@ -11,5 +11,33 @@ buttonInfo.addEventListener("click", ()=>{
     elementToggle(sidebarInfo);
 })
 
+let testimonialsItem=document.querySelectorAll("[data-testimonials-item]");
+let modalContainer=document.querySelector("[data-modal-container]");
+let modalClose=document.querySelector("[data-modal-close-btn]");
+let overlay=document.querySelector("[data-overlay]");
+
+let modalImg=document.querySelector("[data-modal-img]");
+let modalTitle=document.querySelector("[data-modal-title]");
+let modalText=document.querySelector("[data-modal-text]");
+
+let testimonialsModalFunction=function(){
+    modalContainer.classList.toggle("active");
+    overlay.classList.toggle("active");
+}
+
+for(let i=0;i < testimonialsItem.length; i++){
+    testimonialsItem[i].addEventListener("click",function(){
+        
+        modalImg.src=this.querySelector("[data-testimonials-avatar]").src;
+        modalImg.alt=this.querySelector("[data-testimonials-avatar]").alt;
+        modalTitle.innerHTML=this.querySelector("[data-testimonials-title]").innerHTML;
+        modalText.innerHTML=this.querySelector("[data-testimonials-text]").innerHTML;
+
+        testimonialsModalFunction();
+    });
+}
+
+modalClose.addEventListener("click", testimonialsModalFunction);
+overlay.addEventListener("click", testimonialsModalFunction);
 
 
